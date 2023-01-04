c5af4e093a1ac20788da14b713190f01b255bfd0
sueenciso <susana.enciso@icloud.com>
2023-01-03 19:50:47 -0600

    encrypt file

--- src/usecases/user/index.js
+++ src/usecases/user/index.js
@@ -1,21 +1,14 @@
-const User=require("../../models/user").model;
+const User = require("../../models/user").model;
 
-const create = async (email, password)=>{
+const create = async (email, password) => {};
+const update = async (id, password) => {};
 
-}
-const update=async(id,password)=>{
+const findByEmail = async (email) => await User.findOne({ email });
 
-  };
+const authenticate = async (email, password) => {};
 
-  const findByEmail = async (email) => await User.findOne({ email });
-
-const authenticate = async (email, password) => {
-    
-  };
-
-
-  module.exports={
-    create,
-    update,
-    authenticate,
-  }
+module.exports = {
+  create,
+  update,
+  authenticate,
+};
