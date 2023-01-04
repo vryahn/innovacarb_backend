12b49e5829e1b950c4db8152cdba0896173cedea
sueenciso <susana.enciso@icloud.com>
2023-01-03 19:59:40 -0600

    user create module

--- src/usecases/user/index.js
+++ src/usecases/user/index.js
@@ -1,11 +1,28 @@
 const User = require("../../models/user").model;
+const { hashPassword, verifyPassword } = require("../../lib/encrypt");
+const { createToken, verifyToken } = require("../../lib/jwt");
 
-const create = async (email, password) => {};
+const create = async (email, password) => {
+  const hash = await hashPassword(password);
+
+  const user = new User({ email, hash });
+
+  return await user.save();
+};
+
 const update = async (id, password) => {};
 
 const findByEmail = async (email) => await User.findOne({ email });
 
-const authenticate = async (email, password) => {};
+const authenticate = async (email, password) => {
+  const user = await findByEmail(email);
+  const hash = user.hash;
+
+  const isVerified = await verifyPassword(password, hash);
+  if (!isVerified)
+    throw new Error("Ups, tu pass esta mal, intentalo nuevamente");
+  return createToken({ sub: user._id });
+};
 
 module.exports = {
   create,
