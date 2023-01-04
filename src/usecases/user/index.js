d4cab259bea4ae91a97e8d443816dfe7845d2225
sueenciso <susana.enciso@icloud.com>
2023-01-03 19:44:43 -0600

    create USER use case

---
+++ src/usecases/user/index.js
@@ -0,0 +1,21 @@
+const User=require("../../models/user").model;
+
+const create = async (email, password)=>{
+
+}
+const update=async(id,password)=>{
+
+  };
+
+  const findByEmail = async (email) => await User.findOne({ email });
+
+const authenticate = async (email, password) => {
+    
+  };
+
+
+  module.exports={
+    create,
+    update,
+    authenticate,
+  }
