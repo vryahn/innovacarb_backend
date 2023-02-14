f62c78d5919618f92095cab63f0b0a628ac19232
Vryahn <b.ra@live.com.mx>
2023-02-13 22:21:32 -0600

    adding stripe integration

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const cors=require("cors")
 const app = express();
 const apiRouter=require("./src/routes");
 
-
 const config = require("./src/lib/config");
 const db = require("./src/lib/db");
 
@@ -25,3 +24,31 @@ app.get("/", (req, res) => {
       console.error("Connection refused:", err);
     }
   });
+
+// stripe integration
+
+const Stripe = require("stripe");
+const stripe = new Stripe("sk_test_51AyqVLA8JjQ11CV0rRRKk7waYtdCpssgh3bN0YQG7jXX9HklUGfuULv6j2xJnQxI2t4GqJ3pbzwB0jwH8DEn1h2l00icemqelk");
+
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(express.json());
+
+app.post("/api/checkout", async (req, res) => {
+
+  const { id, amount } = req.body;
+
+  try {
+    const payment = await stripe.paymentIntents.create({
+      amount,
+      currency: "USD",
+      description: "Gaming Keyboard",
+      payment_method: id,
+      confirm: true, 
+    });
+
+    return res.status(200).json({ message: "Successful Payment" });
+  } catch (error) {
+    console.log(error);
+    return res.json({ message: error.raw.message });
+  }
+});
