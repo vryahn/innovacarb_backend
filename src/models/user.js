04e1c23b40c1b8def7dec913b6e85142cbf095bd
sueenciso <susana.enciso@icloud.com>
2023-02-14 20:15:19 -0600

    removing property first and last name in Schema User

--- src/models/user.js
+++ src/models/user.js
@@ -4,8 +4,6 @@ const { Schema } = mongoose;
 const schema = new Schema({
   email: { type: String, require: true, unique: true, trim: true },
   hash: { type: String, require: true, trim: true },
-  firstName: { type: String, require: true, trim: true },
-  lastName: { type: String, require: true, trim: true },
   coffeshop: [{type: mongoose.ObjectId, ref: "Coffeshop"}]
 });
 
