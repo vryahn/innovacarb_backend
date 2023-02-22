4d5a8e4a2d678829b4c1aba449ee434208e4e0f5
sueenciso <susana.enciso@icloud.com>
2023-02-22 16:33:57 -0600

    model of quotes

---
+++ src/models/quotes.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const schema = new Schema({
+  dateQuote: { type: Date, require: false, unique: false},
+  statusQuote: {type: Boolean, require: false, unique: false}
+});
+
+const model = mongoose.model("Quote", schema);
+
+module.exports = {
+  schema,
+  model,
+};
