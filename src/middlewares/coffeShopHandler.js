045dc7309ee4b5d6124bbe90189a51d9b732ed03
sueenciso <susana.enciso@icloud.com>
2023-02-21 19:31:54 -0600

    middleware coffeshopehandler

--- src/middlewares/coffeShopHandler.js
+++ src/middlewares/coffeShopHandler.js
@@ -1,8 +1,14 @@
+const User=require("../models/user").model;
+
 const coffeShopeHandler = async (req, res, next) => {
-  const { sub } = req.params.token;
+
+  const { sub } = req.params.token; //sub contiene el id del usuario, para obtener el obj de la bd
+  const cafeteria=req.params.id
   try {
-    
+    User.findById(cafeteria)
+    if(cafeteria.includes(sub)){
     next();
+    }
   } catch (error) {
     const { message } = error;
     res.status(401).json({ ok: false, message });
