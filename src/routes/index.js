40072d720134a8369f376f500d73d9e78eed5166
sueenciso <susana.enciso@icloud.com>
2023-02-23 19:15:33 -0600

    endpoint put in quotesRoutes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const userRoutes = require("./userRoutes");
 const coffeshop = require("./coffeshopRoutes");
 
+
+
 const apiRouter = (app) => {
   app.use("/users", userRoutes);
   app.use("/coffeshop", coffeshop);
---
+++ src/routes/quotesRoutes.js
@@ -0,0 +1,29 @@
+const routes = require("express").Router();
+const {create, updateQuote} = require ("../usecases/quotes");
+
+routes.post("/", async (req,res)=>{
+    const {dateQuote, statusQuote} = req.body;
+
+    try {
+        const payload = await create (dateQuote, statusQuote);
+        res.json({ok: true, message: "Cita creada!", payload})
+    } catch (error){
+        const {message } = error;
+        res.status(500).json({ok: false, message})
+    }
+});
+
+routes.pute("/:id", async (req, res)=>{
+
+    const {id} = req.params;
+    const {dateQuote} = req.body;
+
+    try{
+        const date= await updateQuote(id, dateQuote);
+        res.json({ok: true, payload: date});
+    }catch(error){
+        const {message}= error;
+        res.status(400).json({ok: false, message})
+    }
+});
+
