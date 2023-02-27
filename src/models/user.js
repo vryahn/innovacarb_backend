c7b841205e777513e80cc8b2824c9f56f5ff43b7
sueenciso <susana.enciso@icloud.com>
2023-02-26 19:43:04 -0600

    user model added first and last name

--- src/models/user.js
+++ src/models/user.js
@@ -4,6 +4,8 @@ const { Schema } = mongoose;
 const schema = new Schema({
   email: { type: String, require: true, unique: true, trim: true },
   hash: { type: String, require: true, trim: true },
+  firsName: {type: String, requier: false, trim: false},
+  lastName: {type: String, requier: false, trim: false},
   coffeshop: [{type: mongoose.ObjectId, ref: "Coffeshop"}]
 });
 
