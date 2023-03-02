a1b28e1f944fe62309af32d6c6dbe8bcc43b2be1
sueenciso <susana.enciso@icloud.com>
2023-03-01 21:31:18 -0600

    adminHandeler

--- src/middlewares/adminHandler.js
+++ src/middlewares/adminHandler.js
@@ -1,11 +1,10 @@
 const User = require("../usecases/user");
 
 const adminHandler = async (req, res, next) => {
-  const { sub, rol } = req.params.token; //sub contiene el id del usuario, para obtener el obj de la bd
+  const { rol } = req.params.token; //sub contiene el id del usuario, para obtener el obj de la bd
 
-  const result = await User.getOneUser(sub);
-  if (result.rol == rol && result.rol == "admin") {
-    console.log("resultado: ", result);
+  if (rol == "customer") {
+  
     next();
   } else {
     res.status(401).json({ ok: false, message: "ups, no eres admin" });
